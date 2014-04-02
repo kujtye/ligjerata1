488726d758581be1bc9e06ddb01defab174774e6
kujtye <kujtim@artmotion.net>
2014-04-02 19:44:15 +0200

    initial2

--- src/projekti.java
+++ src/projekti.java
@@ -1,6 +1,19 @@
 
 public class projekti {
 	public static void main(String[] args) {
-		System.out.println("Projekti jone i pare");
-	}
+		
+		int i = 0;
+		int j = 5;
+		char ch = 'a';
+		int sum = i +j;
+		String str = "testt";
+		double dbl = 0.0;
+		float flt = 0.0f;
+		
+		for (int i = 0; i < 5; ++i) {
+			System.out.println(str.charAt(i));
+		}
+		
+	
+	
 }
